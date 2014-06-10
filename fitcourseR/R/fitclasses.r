fccaf3982f3996baf05b1075ef7f20fbd32f31fc
Sebastian Funk <sebastian.funk@lshtm.ac.uk>
2014-06-10 13:31:25 +0100

    make example deterministic

--- fitcourseR/R/fitclasses.r
+++ fitcourseR/R/fitclasses.r
@@ -202,7 +202,7 @@ setParameterValues <- function(list.fitparam,new.values) {
 #' 		\item \code{upper} upper truncation vector
 #' 	}
 #' } 
-#' @example inst/examples/example-fitmodel.r
+#' @examples inst/examples/example-fitmodel.r
 fitmodel <- function(name=NULL,state.variables=NULL, list.fitparam=NULL, initialise.state=NULL, simulate.model=NULL, generate.observation=NULL, log.prior.fitparam=NULL, data=NULL, log.likelihood=NULL, distance.ABC=NULL ,gaussian.proposal=list(covmat=NULL,lower=NULL,upper=NULL), verbose=TRUE){
 	
 	# mandatory
