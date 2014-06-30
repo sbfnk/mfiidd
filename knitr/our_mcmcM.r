764d63092dc040dbcd5a665019d24f2f0e52df3e
Sebastian Funk <sf7@princeton.edu>
2014-06-30 19:23:05 +0100

    getting somewhere with MCMC

--- knitr/our_mcmcM.r
+++ knitr/our_mcmcM.r
@@ -1,81 +1,78 @@
-## @knitr our_mcmcM
-## This is a function that takes four parameters:
-## - target: the target distribution, a function that takes one argument
-##           (a vector) and returns the (logged) value of a distribution
-## - theta.init: the initial value of theta, a named vector
-## - covmat.proposal: the covariance matrix of the (Gaussian) proposal distribution,
-##                    in the same order as in the "target" vector
-## - n.iterations: the number of iterations
-## it returns an MCMC trace (value of theta and target(theta) at every MCMC step)
+# @knitr our_mcmcMH
+# This is a function that takes four parameters:
+# - target.dist: the target distribution, a function that takes one
+#   argument (a number) and returns the (logged) value of a
+#   distribution
+# - theta.init: the initial value of theta, a number
+# - proposal.sd: the standard deviation of (Gaussian) proposal
+#   distribution
+# - n.iterations: the number of iterations
+# Any further arguments passed to my_mcmcMH will be passed to the target
+# distribution.
+# The function returns a vector of samples of theta from the target
+# distribution
+my_mcmcMH <- function(target.dist, init.theta, proposal.sd, n.iterations, ...) {
 
-our_mcmcM <- function(target, theta.init, covmat.proposal, n.iterations) {
+    # evaluate the function "target.dist" at "theta.init", and assign to
+    # a variable called target.theta.current. Pass all
+    # extra arguments of my_mcmcMH to the target function
+    target.theta.current <- target.dist(init.theta, ...)
 
-    ## initialise theta
-    theta.current <- theta.init
-    theta.proposed <- theta.init
-
-    ## evaluate the function "target" at "theta.init", and assign to 
-    ## a variable called target.theta.current
-    target.theta.current <- target(theta.current)
-
-    ## initialise trace data.frame
-    trace <- data.frame(target = target.theta.current, t(theta.current))
-
-    ## initialise number of accepted runs (to calculate acceptance rate later)
+    # initialise variables to store the current value of theta, the
+    # vector of samples, and the number of accepted runs
+    theta.current <- init.theta
+    samples <- theta.current
     accepted <- 0
 
     ## run MCMC for n.iteration interations
     for (i.iteration in seq_len(n.iterations)) {
 
-        ## draw a new theta from the (multivariate Gaussian) proposal 
-        ## distribution and assign to a variable called theta.proposed. 
-        ## See "?rmvnorm for help.
-        theta.proposed <- rmvnorm(n = 1, mean = theta.current, 
-                                  sigma = covmat.proposal)
+        # draw a new theta from the (Gaussian) proposal distribution
+        # and assign to a variable called theta.proposed.  See
+        # "?rnorm for more information
+        theta.proposed <- rnorm(n = 1, mean = theta.current,
+                                sd = proposal.sd)
 
-        ## evaluate the function target at the proposed theta and
-        ## assign to a variable called target.theta.proposed
-        target.theta.proposed <- target(theta.proposed)
+        # evaluate the function target.dist at the proposed theta and
+        # assign to a variable called target.theta.proposed, passing
+        # further arguments using '...'
+        target.theta.proposed <- target.dist(theta.proposed, ...)
 
-        ## compute Metropolis ratio (acceptance probability). Since the
-        ## multivariate Gaussian is symmetric, we don't need to consider
-        ## the proposal distribution here
+        # compute Metropolis-Hastings ratio (acceptance probability). Since
+        # the multivariate Gaussian is symmetric, we don't need to consider
+        # the proposal distribution here
         log.acceptance <- target.theta.proposed - target.theta.current
 
-        ## draw random number number between 0 and 1 using "runif" and assign to
-        ## a variable called r.
+        # draw random number number between 0 and 1 using "runif" and assign to
+        # a variable called r.
         r <- runif(1)
 
-        ## calculate acceptance ratio. This is easiest if you assume
-        ## the target function to return the logarithm of the 
-        ## distribution value. Assign the result to a variable called
-        ## log.acceptance
-        log.acceptance <- target.theta.proposed - target.theta.current
-
-        ## test acceptance (using "exp" because we calculated the logarithm of the
-        ## acceptance ratio before
+        # test acceptance by comparing the random number to the
+        # Metropolis-Hastings ratio (acceptance probability) (using
+        # "exp" because we calculated the logarithm of the
+        # Metropolis-Hastings ratio before)
         if (r < exp(log.acceptance)) {
 
-            ## if accepted: update proposed parameter
-            trace <- rbind(trace, c(target = target.theta.proposed, t(theta.proposed)))
-            ## update theta
+            # if accepted:
+            # change the current value of theta to the proposed theta
             theta.current <- theta.proposed
 
-            ## update target
+            # updated the current value of the target
             target.theta.current <- target.theta.proposed
 
-            ## update number of accepted proposals
+            # update number of accepted proposals
             accepted <- accepted + 1
-        } else {
-            ## reject proposed parameter
-            trace <- rbind(trace, c(target = target.theta.current, t(theta.current)))
         }
 
-        ## print current state of chain and acceptance rate
-        cat("chain:", unlist(trace[nrow(trace), ]),
+        # add the current theta to the vector of samples
+        samples <- c(samples, theta.current)
+
+        # print current state of chain and acceptance rate
+        cat("chain:", theta.current,
             "acceptance rate:", accepted / i.iteration, "\n")
 
     }
 
-    return(trace)
+    # return the trace of the chain (i.e., the vector of samples)
+    return(samples)
 }
