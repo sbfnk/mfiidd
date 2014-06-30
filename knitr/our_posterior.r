1dce07d6e426d11e1f43bb9cf17bc82cdd38d643
Sebastian Funk <sbfnk@users.noreply.github.com>
2014-06-30 11:48:42 +0100

    finished introduction session

--- knitr/our_posterior.r
+++ knitr/our_posterior.r
@@ -1,29 +1,25 @@
-## This is a function that takes two parameters:
-## - fitmodel: the model we want to evaluate
-## - theta: the parameter value(s) at which we want to evaluate the posterior
-
-our_posterior <- function(model, theta) {
+## @knitr posterior
+## This is a function that takes 4 arguments:
+## - fitmodel, a fitmodel object that defines the model dynamics,
+##   prior and likelihoods.
+## - theta, a named vector of parameters
+## - state.init,  a named vector of initial conditions
+## - data, the data set we are fitting the model to
+## It should return the posterior for the given model, parameters,
+## initial conditions and data.
+our_posterior <- function(fitmodel, theta, state.init, data) {
 
     ## calculate the fitmodel prior for parameter vector theta using
-    ## model$log.prior
-    prior <- model$log.prior(theta)
+    ## fitmodel$logPrior, and assign to variable log.prior
+    log.prior <- model$logPrior(theta)
 
-    ## initialise the model (using model$initialise.theta and 
-    ## the parameter vector theta)
-    state.init <- model$initialise.state(theta)
-
-    ## simulate the model at all the times given in model$data$time,
-    ## that is at all the times that are in the data, using 
-    ## model$simulate.model, with initial state given by staten.init
-    trajectory <- model$simulate.model(theta, state.init, model$data$time)
-
-    ## calculate the log-likelihood using model$log.likelihood
-    likelihood <- model$log.likelihood(model$data, theta, trajectory)
+    ## calculate the fitmodel prior for parameter vector theta using
+    ## fitmodel$logPrior, and assign to variable log.prior
+    log.likelihood <- trajLogLike(fitmodel, theta, state.init, data)
 
     ## calulate the posterior using the log-prior and log-likelihood
-    posterior <- prior + likelihood
-    
+    posterior <- log.prior + log.likelihood
+
     return(posterior)
 
 }
-
