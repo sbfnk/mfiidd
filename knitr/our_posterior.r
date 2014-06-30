6fd257bec9d3db7842280d386a26380f67112956
Sebastian Funk <sbfnk@users.noreply.github.com>
2014-06-30 15:01:04 +0100

    single hash -- otherwise it's not fontified correctly

--- knitr/our_posterior.r
+++ knitr/our_posterior.r
@@ -1,23 +1,23 @@
 ## @knitr posterior
-## This is a function that takes 4 arguments:
-## - fitmodel, a fitmodel object that defines the model dynamics,
-##   prior and likelihoods.
-## - theta, a named vector of parameters
-## - state.init,  a named vector of initial conditions
-## - data, the data set we are fitting the model to
-## It should return the posterior for the given model, parameters,
-## initial conditions and data.
-our_posterior <- function(fitmodel, theta, state.init, data) {
+# This is a function that takes 4 arguments:
+# - fitmodel, a fitmodel object that defines the model dynamics,
+#   prior and likelihoods.
+# - theta, a named vector of parameters
+# - state.init,  a named vector of initial conditions
+# - data, the data set we are fitting the model to
+# It should return the posterior for the given model, parameters,
+# initial conditions and data.
+my_posterior <- function(fitmodel, theta, state.init, data) {
 
-    ## calculate the fitmodel prior for parameter vector theta using
-    ## fitmodel$logPrior, and assign to variable log.prior
-    log.prior <- model$logPrior(theta)
+    # calculate the fitmodel prior for parameter vector theta using
+    # fitmodel$logPrior, and assign to variable log.prior
+    log.prior <- fitmodel$logPrior(theta)
 
-    ## calculate the fitmodel prior for parameter vector theta using
-    ## fitmodel$logPrior, and assign to variable log.prior
+    # calculate the fitmodel prior for parameter vector theta using
+    # fitmodel$logPrior, and assign to variable log.prior
     log.likelihood <- trajLogLike(fitmodel, theta, state.init, data)
 
-    ## calulate the posterior using the log-prior and log-likelihood
+    # calulate the posterior using the log-prior and log-likelihood
     posterior <- log.prior + log.likelihood
 
     return(posterior)
