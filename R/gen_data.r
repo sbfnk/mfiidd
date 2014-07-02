381e6f55a434a95b0f2c757047cd64aa51abe3c2
Sebastian Funk <sbfnk@users.noreply.github.com>
2014-07-02 01:15:33 +0100

    update parameter names

--- R/gen_data.r
+++ R/gen_data.r
@@ -2,14 +2,14 @@ library(fitR)
 example(SIR)
 example(SIR_reporting)
 
-parameters <- c(R0 = 1.8, D = 2)
+parameters <- c(R0 = 1.8, D.inf = 2)
 state.init <- c(S = 999, I = 1, R = 0)
 
 epi1 <- genObsTraj(SIR, parameters, state.init, 1:100)
 epi1 <- epi1[1:38, ]
 epi1 <- epi1[c("time", "obs")]
 
-parameters <- c(R0 = 2.5, D = 2, RR = 0.1)
+parameters <- c(R0 = 2.5, D.inf = 2, RR = 0.1)
 epi2 <- genObsTraj(SIR_reporting, parameters, state.init, 1:100)
 epi2 <- epi1[1:26, ]
 epi2 <- epi1[c("time", "obs")]
