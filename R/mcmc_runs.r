5fefb370fa49a9e93b31bdc8f426fb881ba893a5
Sebastian Funk <sbfnk@users.noreply.github.com>
2014-07-04 08:57:39 +0100

    theta.init -> init.theta

--- R/mcmc_runs.r
+++ R/mcmc_runs.r
@@ -7,8 +7,8 @@ my_logPosterior_epi1 <- function(theta) {
 
 }
 
-## mcmc.run.R0 <- mcmcMH(target = my_logPosterior_epi1, theta.init = c(R0 = 3, D.inf = 2), proposal.sd = c(0.05, 0), n.iterations = 10000)
-mcmc.epi1 <- mcmcMH(target = my_logPosterior_epi1, theta.init = c(R0 = 3, D.inf = 2), proposal.sd = c(0.05, 0), n.iterations = 10000)
+## mcmc.run.R0 <- mcmcMH(target = my_logPosterior_epi1, init.theta = c(R0 = 3, D.inf = 2), proposal.sd = c(0.05, 0), n.iterations = 10000)
+mcmc.epi1 <- mcmcMH(target = my_logPosterior_epi1, init.theta = c(R0 = 3, D.inf = 2), proposal.sd = c(0.05, 0), n.iterations = 10000)
 
 my_logPosterior_epi3 <- function(theta) {
 
@@ -19,8 +19,8 @@ my_logPosterior_epi3 <- function(theta) {
 
 }
 
-## mcmc.run.R0.Dinf <- mcmcMH(target = my_logPosterior_epi3, theta.init = c(R0 = 1, D.inf = 2), proposal.sd = c(0.01, 0.1), n.iterations = 10000)
-mcmc.epi3 <- mcmcMH(target = my_logPosterior_epi3, theta.init = c(R0 = 1, D.inf = 2), proposal.sd = c(0.01, 0.1), n.iterations = 1000)
+## mcmc.run.R0.Dinf <- mcmcMH(target = my_logPosterior_epi3, init.theta = c(R0 = 1, D.inf = 2), proposal.sd = c(0.01, 0.1), n.iterations = 10000)
+mcmc.epi3 <- mcmcMH(target = my_logPosterior_epi3, init.theta = c(R0 = 1, D.inf = 2), proposal.sd = c(0.01, 0.1), n.iterations = 1000)
 
 my_logPosterior_epi4 <- function(theta) {
 
@@ -31,7 +31,7 @@ my_logPosterior_epi4 <- function(theta) {
 
 }
 
-## mcmc.run.R0.Dinf.RR <- mcmcMH(target = my_logPosterior_epi4, theta.init = c(R0 = 1, D.inf = 2, RR = 1), proposal.sd = c(0.01, 0.1, 0.01), n.iterations = 10000)
-mcmc.epi4 <- mcmcMH(target = my_logPosterior_epi4, theta.init = c(R0 = 1, D.inf = 2, RR = 1), proposal.sd = c(0.01, 0.1, 0.01), n.iterations = 10000)
+## mcmc.run.R0.Dinf.RR <- mcmcMH(target = my_logPosterior_epi4, init.theta = c(R0 = 1, D.inf = 2, RR = 1), proposal.sd = c(0.01, 0.1, 0.01), n.iterations = 10000)
+mcmc.epi4 <- mcmcMH(target = my_logPosterior_epi4, init.theta = c(R0 = 1, D.inf = 2, RR = 1), proposal.sd = c(0.01, 0.1, 0.01), n.iterations = 10000)
 
 save(mcmc.epi1, mcmc.epi3, mcmc.epi4, file = "mcmc.rdata")
