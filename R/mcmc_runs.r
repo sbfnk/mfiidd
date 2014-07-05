c05ace86555e041ae1878a6cc42898d04d5bf0d1
Sebastian Funk <sbfnk@users.noreply.github.com>
2014-07-05 21:32:54 +0100

    modified logPrior

--- R/mcmc_runs.r
+++ R/mcmc_runs.r
@@ -35,3 +35,17 @@ my_logPosterior_epi4 <- function(theta) {
 mcmc.epi4 <- mcmcMH(target = my_logPosterior_epi4, init.theta = c(R0 = 1, D.inf = 2, RR = 1), proposal.sd = c(0.01, 0.1, 0.01), n.iterations = 10000)
 
 save(mcmc.epi1, mcmc.epi3, mcmc.epi4, file = "mcmc.rdata")
+
+SEIT2L_sto$logPrior <- function(theta) {
+
+	log.prior.R0 <- dunif(theta[["R0"]], min = 1, max = 50, log = TRUE)
+	log.prior.latent.period <- dnorm(theta[["D.lat"]], mean = 2, log = TRUE)
+	log.prior.infectious.period <- dnorm(theta[["D.inf"]], mean = 2, log = TRUE)
+	log.prior.temporary.immune.period <- dunif(theta[["D.imm"]], min = 0, max = 50, log = TRUE)
+	log.prior.probability.long.term.immunity <- dunif(theta[["alpha"]], min = 0, max = 1, log = TRUE)
+	log.prior.reporting.rate <- dunif(theta[["rho"]], min = 0, max = 1, log = TRUE)
+	
+	return(log.prior.R0 + log.prior.latent.period + log.prior.infectious.period + log.prior.temporary.immune.period + log.prior.probability.long.term.immunity + log.prior.reporting.rate)
+
+}
+
