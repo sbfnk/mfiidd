6f7fc78a34cd8b1c53ee0d955269843b780e98e5
Anton <anton.camacho@lshtm.ac.uk>
2014-11-24 14:38:48 +0100

    regenerate introduction

--- R/generate_html.R
+++ R/generate_html.R
@@ -1,15 +1,23 @@
-generate_html <- function() {
+generate_html <- function(Rmd_name) {
 
 	library(rmarkdown)
 	library(knitr)
 
-	render("/Users/Tonton/edu/Fit_course/mfiidd/Rmd_files/index.Rmd",output_format=html_document(toc=TRUE, fig_width=12, fig_height=8, theme="flatly"))
+	repo_dir <- "/Users/Tonton/edu/Fit_course/mfiidd"
+	input_dir <- file.path(repo_dir, "Rmd")
+	output_dir <- file.path(repo_dir, "html")
+
+	for(i in Rmd_name){
+		render(file.path(input_dir,sprintf("%s.Rmd",i)),output_dir=output_dir,output_format=html_document(toc=TRUE, fig_width=5, fig_height=5, theme="flatly"))		
+	}
 
 }
 
 main <- function() {
 
-	generate_html()
+	Rmd_name <- c("index","introduction")
+
+	generate_html(Rmd_name)
 
 }
 
