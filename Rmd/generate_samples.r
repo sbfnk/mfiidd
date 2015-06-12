3063b6457b6b40b5150150be4837290d9cb5d11e
sbfnk <sbfnk@users.noreply.github.com>
2015-06-12 08:19:40 +0100

    D. -> D_ for better pomp integrability

--- Rmd/generate_samples.r
+++ Rmd/generate_samples.r
@@ -1,8 +1,8 @@
 # @knitr generate_samples
 trace <- my_rmcmcMH(target = my_dLogPosterior_epi1, # target distribution
-                   init.theta = c(R0 = 1, D.inf = 2), # intial parameter guess
+                   init.theta = c(R0 = 1, D_inf = 2), # intial parameter guess
                    proposal.sd = c(0.1, 0), # standard deviation of
                        # Gaussian proposals: 0.1 for the first
                        # parameter of init.theta (R0), 0 for the
-                       # second (D.inf,  which we keep fixed)
+                       # second (D_inf,  which we keep fixed)
                    n.iterations = 1000) # number of iterations
