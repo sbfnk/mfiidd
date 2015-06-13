d70d29e09b074c13968e0cddc764d186de81c097
Sebastian Funk <sbfnk@users.noreply.github.com>
2015-06-13 11:17:25 +0100

    compile all markdown

--- Rmd/compile_markdown.R
+++ Rmd/compile_markdown.R
@@ -1,9 +1,12 @@
 mfiidd.dir <- "~/teaching/mfiidd" ## Seb
 
+library('rmarkdown')
 library('coda')
+library('plyr')
 library('dplyr')
 library('lattice')
 library('ggplot2')
+library('reshape2')
 
 Rmd.files <- list.files(path.expand(paste0(mfiidd.dir, "/Rmd/")), ".*\\.Rmd$",
                         full.names = TRUE)
