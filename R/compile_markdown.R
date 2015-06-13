bd061055124517be2a4f77f2b52a0b326002b8bb
Anton <anton.camacho@lshtm.ac.uk>
2015-06-13 17:46:53 +0100

    add spec

--- R/compile_markdown.R
+++ R/compile_markdown.R
@@ -12,13 +12,15 @@ library('ggplot2')
 library('reshape2')
 
 ## first reinstall latest release of fitR so it is the one used in "render()"
-install_github("sbfnk/fitR")
+# install_github("sbfnk/fitR")
 
 ## recompile all html files
 Rmd.files <- list.files(path.expand(paste0(mfiidd.dir, "/Rmd/")), ".*\\.Rmd$",
                         full.names = TRUE)
 
+# Rmd.files <- path.expand(paste0(mfiidd.dir, "/Rmd/introduction.Rmd"))
+
 for (file in Rmd.files)
 {
-    render(file, output_dir = path.expand(paste0(mfiidd.dir, "/website/")), output_format=html_document(toc=TRUE, fig_width=5, fig_height=5, theme="flatly"))
+    render(file, output_dir = path.expand(paste0(mfiidd.dir, "/website/")), output_format=html_document(smart=FALSE, toc=TRUE, fig_width=5, fig_height=5, theme="spacelab", highlight="pygments"))
 }
