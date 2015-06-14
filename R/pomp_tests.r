f7f6d5d2fa394d8ffa1045b52a17ecea6ebb9aa1
Sebastian Funk <sbfnk@users.noreply.github.com>
2015-06-14 15:33:06 +0100

    more pomping

--- R/pomp_tests.r
+++ R/pomp_tests.r
@@ -1,33 +1,29 @@
 data(SEITL_pomp)
 data(SEIT2L_pomp)
 
-theta.bad.guess <- c(R0 = 2, D_lat = 2, D_inf = 2, alpha = 0.9, D_imm = 13, rho = 0.85)
-init.state.bad.guess <- c(S = 250, E = 0, I = 4, T = 0, L = 30, Inc = 0)
-init.state.bad.guess2 <- c(S = 250, E = 0, I = 4, T1 = 0, T2 = 0, L = 30, Inc = 0)
-
-pomp.init.state.bad.guess <- init.state.bad.guess
-pomp.init.state.bad.guess2 <- init.state.bad.guess2
-
-names(pomp.init.state.bad.guess) <- paste(names(init.state.bad.guess), 0, sep = ".")
-names(pomp.init.state.bad.guess2) <- paste(names(init.state.bad.guess2), 0, sep = ".")
+theta <-
+    c(R0 = 2, D_lat = 2, D_inf = 2, alpha = 0.9, D_imm = 13, rho = 0.85)
+SEITL.init.state <-
+    c(S.0 = 250, E.0 = 0, I.0 = 4, T.0 = 0, L.0 = 30, Inc.0 = 0)
+SEIT2L.init.state <-
+    c(S.0 = 250, E.0 = 0, I.0 = 4, T1.0 = 0, T2.0 = 0, L.0 = 30, Inc.0 = 0)
 
 ## deterministic trajectory
-traj <- trajectory(SEITL_pomp,
-                   params = c(theta.bad.guess, pomp.init.state.bad.guess),
-                   state = TRUE, obs = TRUE)
+traj <- trajectory(SEITL_pomp, params = c(theta, SEITL.init.state),
+                   as.data.frame = TRUE)
 traj2 <- trajectory(SEIT2L_pomp,
-                    params = c(theta.bad.guess, pomp.init.state.bad.guess),
+                    params = c(theta, SEIT2L.init.state),
                     state = TRUE, obs = TRUE)
 
 ## stochastic simulation
-sim1 <- simulate(SEITL_pomp, params = c(theta.bad.guess, pomp.init.state.bad.guess))
-sim2 <- simulate(SEIT2L_pomp, params = c(theta.bad.guess, pomp.init.state.bad.guess))
+sim1 <- simulate(SEITL_pomp, params = c(theta, pomp.init.state))
+sim2 <- simulate(SEIT2L_pomp, params = c(theta, pomp.init.state))
 
 ## trajectory matching
-tm <- traj.match(SEITL_pomp,start = c(theta.bad.guess, pomp.init.state.bad.guess),
-                 est = names(theta.bad.guess))
-tm2 <- traj.match(SEIT2L_pomp,start=c(theta.bad.guess, pomp.init.state.bad.guess2),
-                  est = names(theta.bad.guess))
+tm <- traj.match(SEITL_pomp,start = c(theta, pomp.init.state),
+                 est = names(theta))
+tm2 <- traj.match(SEIT2L_pomp,start=c(theta, pomp.init.state2),
+                  est = names(theta))
 
 tm_sim <- simulate(mf, nsim = 10, as.data.frame = TRUE, include.data = TRUE)
 tm_sim2 <- simulate(mf2, nsim = 10, as.data.frame = TRUE, include.data = TRUE)
@@ -43,7 +39,7 @@ p2 <- p2 + scale_alpha_manual("", values = c(`TRUE` = 1, `FALSE` = 0.2),
 p2 <- p2 + geom_line()
 
 ## MIF
-prop.sd <- theta.bad.guess
+prop.sd <- theta
 prop.sd[] <- 0.01
 
 mf <- mif(tm, Nmif = 50, Np = 1000, cooling.fraction = 0.8, rw.sd = prop.sd)
@@ -76,8 +72,8 @@ system.time(pm50 <- foreach (i=1:2, .combine=c, .options.multicore = mcopts) %do
             { pmcmc(mf, Nmcmc = 3000, Np = 50, proposal = mvn.diag.rw(prop.sd),
                     verbose = TRUE, max.fail = Inf)
             })
-traces <- conv.rec(pm50,names(theta.bad.guess))
-prop.sd.new <- apply(sapply(names(theta.bad.guess), function(x) sapply(traces[, x], sd)), 2, mean)
+traces <- conv.rec(pm50,names(theta))
+prop.sd.new <- apply(sapply(names(theta), function(x) sapply(traces[, x], sd)), 2, mean)
 
 accRate <- 1 - rejectionRate(traces)
 
@@ -85,7 +81,7 @@ system.time(pm50 <- foreach (i=1:2, .combine=c, .options.multicore = mcopts) %do
             { pmcmc(mf, Nmcmc = 3000, Np = 50, proposal = mvn.diag.rw(prop.sd.new),
                     verbose = TRUE, max.fail = Inf)
             })
-traces <- conv.rec(pm50,names(theta.bad.guess))
+traces <- conv.rec(pm50,names(theta))
 rejectionRate(traces)
 plot(traces[, "R0"])
 
@@ -93,8 +89,8 @@ system.time(pm400 <- foreach (i=1:2, .combine=c, .options.multicore = mcopts) %d
             { pmcmc(mf, Nmcmc = 3000, Np = 400, proposal = mvn.diag.rw(prop.sd),
                     verbose = TRUE, max.fail = Inf)
             })
-traces <- conv.rec(pm400,names(theta.bad.guess))
-prop.sd.new <- apply(sapply(names(theta.bad.guess), function(x) sapply(traces[, x], sd)), 2, mean)
+traces <- conv.rec(pm400,names(theta))
+prop.sd.new <- apply(sapply(names(theta), function(x) sapply(traces[, x], sd)), 2, mean)
 
 accRate <- 1 - rejectionRate(traces)
 
@@ -102,7 +98,7 @@ system.time(pm400 <- foreach (i=1:2, .combine=c, .options.multicore = mcopts) %d
             { pmcmc(mf, Nmcmc = 3000, Np = 400, proposal = mvn.diag.rw(prop.sd.new),
                     verbose = TRUE, max.fail = Inf)
             })
-traces <- conv.rec(pm400,names(theta.bad.guess))
+traces <- conv.rec(pm400,names(theta))
 rejectionRate(traces)
 plot(traces[, "R0"])
 
