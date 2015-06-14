a9b322de81a019feab835a569dbd496172cc43b9
Sebastian Funk <sbfnk@users.noreply.github.com>
2015-06-14 22:14:27 +0100

    need the Rmd file

--- R/compile_markdown.R
+++ R/compile_markdown.R
@@ -24,6 +24,6 @@ for (file in Rmd.files) {
 	html.file <- path.expand(paste0(mfiidd.dir, "/", "website/",
 		sub("\\.Rmd$", ".html", file)))
 	if (!file.exists(html.file) | file.mtime(Rmd.file) > file.mtime(html.file)) {
-		render(file, output_dir = path.expand(paste0(mfiidd.dir, "/website/")), output_format=html_document(smart=FALSE, toc=TRUE, fig_width=5, fig_height=5, theme="spacelab", highlight="pygments"))
+		render(Rmd.file, output_dir = path.expand(paste0(mfiidd.dir, "/website/")))
 	}
 }
