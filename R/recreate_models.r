e300bd8e5bf05022788d8209e694222ec384dd50
Anton <anton.camacho@lshtm.ac.uk>
2015-06-15 23:51:28 +0100

    create SEIT4L model

--- R/recreate_models.r
+++ R/recreate_models.r
@@ -29,3 +29,9 @@ save(SEIT2L_deter, file = path.expand(paste0(fitR.dir, "/", "data/SEIT2L_deter.r
 source(path.expand(paste0(fitR.dir, "/", "inst/examples/example-SEIT2L-sto.r")))
 save(SEIT2L_stoch, file = path.expand(paste0(fitR.dir, "/", "data/SEIT2L_stoch.rdata")))
 
+source(path.expand(paste0(fitR.dir, "/", "inst/examples/example-SEIT4L-deter.r")))
+save(SEIT4L_deter, file = path.expand(paste0(fitR.dir, "/", "data/SEIT4L_deter.rdata")))
+
+source(path.expand(paste0(fitR.dir, "/", "inst/examples/example-SEIT4L-sto.r")))
+save(SEIT4L_stoch, file = path.expand(paste0(fitR.dir, "/", "data/SEIT4L_stoch.rdata")))
+
