cc4b5f2245ffee4638148d921aa25848665ab357
Sebastian Funk <sebastian.funk@lshtm.ac.uk>
2023-06-08 12:26:25 +0100

    restyle

--- Rmd/epi3_wrapper.r
+++ Rmd/epi3_wrapper.r
@@ -1,9 +1,9 @@
 # @knitr epi3_wrapper
 my_dLogPosteriorEpi3 <- function(theta) {
-
-    return(my_dLogPosterior(fitmodel = SIR,
-                            theta = theta,
-                            initState = c(S = 999, I = 1, R = 0),
-                            data = epi3))
-
+  return(my_dLogPosterior(
+    fitmodel = SIR,
+    theta = theta,
+    initState = c(S = 999, I = 1, R = 0),
+    data = epi3
+  ))
 }
