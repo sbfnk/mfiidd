a3c4074bdc385206891ba2913e0a51e33afca8e4
Sebastian Funk <sebastian.funk@lshtm.ac.uk>
2023-06-15 22:54:06 +0100

    create the 4l model too

--- scripts/generate-data/generate-seitl-mcmc.r
+++ scripts/generate-data/generate-seitl-mcmc.r
@@ -9,7 +9,6 @@ source(here::here("scripts", "snippets", "set-mcmc.r"))
 source(here::here("scripts", "snippets", "run-mcmc.r"))
 
 mcmcSeitl <- my_mcmcTdc
-save(mcmcSeitl, file = here::here("data", "mcmcTdcDeterShortRun.rdata"))
 
 source(here::here("scripts", "snippets", "theta-init.r"))
 nIterations <- 50000
@@ -34,8 +33,53 @@ initTheta <- theta2
 source(here::here("scripts", "snippets", "run-mcmc.r"))
 mcmcSeitlInfoPriorTheta2 <- my_mcmcTdc
 
+## SEIT4L model
+tmp <- readLines(setPmcmcScript)
+tmp <- sub("my_fitmodel <- .*$", "my_fitmodel <- seit4lDeter", tmp)
+tmp <- sub(
+  "my_initState <- .*$",
+  paste0(
+    "my_initState <- ",
+    "c(S = 279, E = 0, I = 2, T1 = 3, T2 = 0, T3 = 0, T4 = 0, L = 0, Inc = 0)"
+  ), tmp)
+source(textConnection(tmp))
+
+source(here::here("scripts", "snippets", "set-mcmc.r"))
+source(here::here("scripts", "snippets", "run-mcmc.r"))
+
+mcmcSeit4l <- my_mcmcTdc
+
+source(here::here("scripts", "snippets", "theta-init.r"))
+nIterations <- 50000
+
+initTheta <- theta1
+source(here::here("scripts", "snippets", "run-mcmc.r"))
+mcmcSeit4lTheta1 <- my_mcmcTdc
+
+initTheta <- theta2
+source(here::here("scripts", "snippets", "run-mcmc.r"))
+mcmcSeit4lTheta2 <- my_mcmcTdc
+
+## informative priors
+source(here::here("scripts", "snippets", "seitl-info-prior.r"))
+seitlDeter$dPrior <- seitlInfoPrior
+
+initTheta <- theta1
+source(here::here("scripts", "snippets", "run-mcmc.r"))
+mcmcSeit4lInfoPriorTheta1 <- my_mcmcTdc
+
+initTheta <- theta2
+source(here::here("scripts", "snippets", "run-mcmc.r"))
+mcmcSeit4lInfoPriorTheta2 <- my_mcmcTdc
+
+save(
+  mcmcSeitl, mcmcSeit4l, file = here::here("data", "mcmcTdcDeterShortRun.rdata")
+)
+
 save(
   mcmcSeitlTheta1, mcmcSeitlTheta2,
   mcmcSeitlInfoPriorTheta1, mcmcSeitlInfoPriorTheta2,
+  mcmcSeit4lTheta1, mcmcSeit4lTheta2,
+  mcmcSeit4lInfoPriorTheta1, mcmcSeit4lInfoPriorTheta2,
   file = here::here("data", "mcmcTdcDeterLongRun.rdata")
 )
