97519da11012902e7d2f58998376ecc5ec428008
Sebastian Funk <sebastian.funk@lshtm.ac.uk>
2023-10-03 13:47:41 +0100

    update to pomp 2 syntax

--- Rmd/pomp/seitlPompR.r
+++ Rmd/pomp/seitlPompR.r
@@ -1,96 +1,87 @@
-seitlSkelR <- function(x, t, params, ...) {
+seitlSkelR <- function(S, E, I, T, L, Inc, R0, D_inf, D_lat, D_imm, alpha,
+                       rho, ...) {
+  beta <- R0 / D_inf
+  epsilon <- 1 / D_lat
+  nu <- 1 / D_inf
+  tau <- 1 / D_imm
+  N <- S + E + I + T + L
 
-  with(as.list(c(x, params)), {
-    beta <- R0 / D_inf
-    epsilon <- 1 / D_lat
-    nu <- 1 / D_inf
-    tau <- 1 / D_imm
-    N <- S + E + I + T + L
+  trans <- c(
+    beta * I / N * S,
+    epsilon * E,
+    nu * I,
+    alpha * tau * T,
+    (1 - alpha) * tau * T
+  )
 
-    trans <- c(
-      beta * I / N * S,
-      epsilon * E,
-      nu * I,
-      alpha * tau * T,
-      (1 - alpha) * tau * T
-    )
+  DS <- -trans[1] + trans[5]
+  DE <- trans[1] - trans[2]
+  DI <- trans[2] - trans[3]
+  DT <- trans[3] - trans[4] - trans[5]
+  DL <- trans[4]
+  DInc <- trans[2]
 
-    DS <- -trans[1] + trans[5]
-    DE <- trans[1] - trans[2]
-    DI <- trans[2] - trans[3]
-    DT <- trans[3] - trans[4] - trans[5]
-    DL <- trans[4]
-    DInc <- trans[2]
-
-    c(DS, DE, DI, DT, DL, DInc)
-  })
+  c(S = DS, E = DE, I = DI, T = DT, L = DL, Inc = DInc)
 }
 
-seitlSimR <- function(x, t, params, delta.t, ...) {
-  with(as.list(c(x, params)), {
-    beta = R0 / D_inf
-    epsilon = 1 / D_lat
-    nu = 1 / D_inf
-    tau = 1 / D_imm
+seitlSimR <- function(S, E, I, T, L, Inc, R0, D_inf, D_lat, D_imm, alpha, rho,
+                      delta.t, ...) {
+  beta <- R0 / D_inf
+  epsilon <- 1 / D_lat
+  nu <- 1 / D_inf
+  tau <- 1 / D_imm
 
-    N = S + E + I + T + L
+  N <- S + E + I + T + L
 
-    rate <- c(
-      beta * I / N,
-      epsilon,
-      nu,
-      alpha * tau,
-      (1 - alpha) * tau
-    )
+  rate <- c(
+    beta * I / N,
+    epsilon,
+    nu,
+    alpha * tau,
+    (1 - alpha) * tau
+  )
 
-    dN <- c(
-      reulermultinom(n = 1, size = S, rate = rate[1], dt = delta.t),
-      reulermultinom(n = 1, size = E, rate = rate[2], dt = delta.t),
-      reulermultinom(n = 1, size = I, rate = rate[3], dt = delta.t),
-      reulermultinom(n = 2, size = T, rate = rate[4:5], dt = delta.t)
-    )
+  dN <- c(
+    reulermultinom(n = 1, size = S, rate = rate[1], dt = delta.t),
+    reulermultinom(n = 1, size = E, rate = rate[2], dt = delta.t),
+    reulermultinom(n = 1, size = I, rate = rate[3], dt = delta.t),
+    reulermultinom(n = 2, size = T, rate = rate[4:5], dt = delta.t)
+  )
 
-    S <- S - dN[1] + dN[5]
-    E <- E + dN[1] - dN[2]
-    I <- I + dN[2] - dN[3]
-    T <- T + dN[3] - dN[4] - dN[5]
-    L <- L + dN[4]
-    Inc <- Inc + dN[2]
+  S <- S - dN[1] + dN[5]
+  E <- E + dN[1] - dN[2]
+  I <- I + dN[2] - dN[3]
+  T <- T + dN[3] - dN[4] - dN[5]
+  L <- L + dN[4]
+  Inc <- Inc + dN[2]
 
-    c(S, E, I, T, L, Inc)
-  })
+  c(S = S, E = E, I = I, T = T, L = L, Inc = Inc)
 }
 
-seitlGenObsPointR <- function(x, t, params, ...) {
-  with(as.list(c(x, params)), {
-    c(obs = rpois(n = 1, ifelse(rho * Inc > 0, rho * Inc, 0)))
-  })
+seitlGenObsPointR <- function(Inc, rho, ...) {
+  c(obs = rpois(n = 1, ifelse(rho * Inc > 0, rho * Inc, 0)))
 }
 
-seitlPointLikeR <- function(y, x, t, params, log, ...) {
-  with(as.list(c(y, x, params)), {
-    dpois(obs, ifelse(rho * Inc > 0, rho * Inc, 0), log)
-  })
+seitlPointLikeR <- function(obs, Inc, rho, ..., log) {
+  dpois(obs, ifelse(rho * Inc > 0, rho * Inc, 0), log)
 }
 
-seitlPriorR <- function(params, log = FALSE, ...) {
-  with(as.list(c(params)), {
-    lik <- dunif(R0, 1, 50, log = TRUE) +
-      dunif(D_lat, 0, 10, log = TRUE) +
-      dunif(D_inf, 0, 15, log = TRUE) +
-      dunif(D_imm, 0, 50, log = TRUE) +
-      dunif(alpha, 0, 1, log = TRUE) +
-      dunif(rho, 0, 1, log = TRUE)
+seitlPriorR <- function(R0, D_lat, D_inf, D_imm, alpha, rho, ..., log = FALSE) {
+  lik <- dunif(R0, 1, 50, log = TRUE) +
+    dunif(D_lat, 0, 10, log = TRUE) +
+    dunif(D_inf, 0, 15, log = TRUE) +
+    dunif(D_imm, 0, 50, log = TRUE) +
+    dunif(alpha, 0, 1, log = TRUE) +
+    dunif(rho, 0, 1, log = TRUE)
 
-    ifelse(log, lik, exp(lik))
-  })
+  ifelse(log, lik, exp(lik))
 }
 
 ## construct pomp object
 seitlPompR <- pomp(
   data = fluTdc1971[, c("time", "obs")],
   skeleton = vectorfield(seitlSkelR),
-  rprocess = euler(step.fun = seitlSimR, delta.t = 0.1),
+  rprocess = discrete_time(step.fun = seitlSimR, delta.t = 0.1),
   rmeasure = seitlGenObsPointR,
   dmeasure = seitlPointLikeR,
   dprior = seitlPriorR,
